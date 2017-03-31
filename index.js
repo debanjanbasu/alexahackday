66b97a71afc382e3f9dcf34d0457bf89afe8f5f5
Debanjan Basu <debanjan.basu@fairfaxmedia.com.au>
2017-03-31 12:08:36 +1100

    added alexa code - initial commit

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 'use strict';
 const APP_ID = 'amzn1.ask.skill.1bcb51a5-fa1e-4dbc-8700-570e25714829';
-const SPEECH_OUTPUT = 'Hello Dev, and a warm welcome to all of those from fairfax attending todays Alexa Hackday';
+// const SPEECH_OUTPUT = 'Hello Dev, and a warm welcome to all of those from fairfax attending todays Alexa Hackday';
+let sharePrice;
 const AlexaSkill = require('./AlexaSkill');
 const GreeterService = function() {
     AlexaSkill.call(this, APP_ID);
 };
 
 GreeterService.prototype = Object.create(AlexaSkill.prototype);
-const helloResponseFunction = function(intent, session, response) {
+const shareDataShout = function(intent, session, response) {
     response.tell(SPEECH_OUTPUT);
 };
 
-GreeterService.prototype.eventHandlers.onLaunch = helloResponseFunction;
+GreeterService.prototype.eventHandlers.onLaunch = shareDataShout;
 
 GreeterService.prototype.intentHandlers = {
-    'HelloWorldIntent': helloResponseFunction
+    'HelloWorldIntent': shareDataShout
 };
 
 exports.handler = function(event, context) {
